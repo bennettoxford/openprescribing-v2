4fdf77e1b4a971f6b618cc8324d98a5a0691aed0
Peter Inglesby <peter.inglesby@gmail.com>
2025-11-07 15:20:55 +0000

    Add skeletal view

--- openprescribing/settings.py
+++ openprescribing/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "openprescribing",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
