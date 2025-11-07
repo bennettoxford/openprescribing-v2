d2ccaa3dbe309efdf2be2dc02a88b36ca6f39cf1
David Evans <dave.evans@thedatalab.org>
2025-11-07 17:09:08 +0000

    Add "no cover" to files we don't intend to test right now

--- openprescribing/config/wsgi.py
+++ openprescribing/config/wsgi.py
@@ -6,6 +6,8 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
+# We expect this will get covered when we add some browser tests
+# pragma: no cover file
 
 import os
 
