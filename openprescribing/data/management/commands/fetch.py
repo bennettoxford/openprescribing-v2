8803b8339d523e44e549dc687c5c65587b625651
David Evans <dave.evans@thedatalab.org>
2025-11-10 16:04:49 +0000

    Add log handling to `fetch` command

--- openprescribing/data/management/commands/fetch.py
+++ openprescribing/data/management/commands/fetch.py
@@ -1,3 +1,6 @@
+import datetime
+import logging
+
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
@@ -25,6 +28,49 @@ class Command(BaseCommand):
     def handle(self, fetcher_names, **options):
         if "all" in fetcher_names:
             fetcher_names = self.available_fetchers.keys()
+
+        log_handler = LogHandler(
+            self.stdout.write,
+            # Knowing the length of the names upfront allows us to produce more readable
+            # logs by justifying them correctly
+            max_name_width=max(len(name) for name in fetcher_names),
+        )
+
         for name in fetcher_names:
             fetcher = self.available_fetchers[name]
-            fetcher(settings.DOWNLOAD_DIR)
+            with log_handler.capture_logs_as(name):
+                fetcher(settings.DOWNLOAD_DIR)
+
+
+class LogHandler:
+    def __init__(self, writer, max_name_width=0):
+        self.writer = writer
+        self.max_name_width = max_name_width
+
+        root_module = __name__.partition(".")[0]
+        self.logger = logging.getLogger(root_module)
+        self.log_handler = logging.Handler()
+        self.log_handler.emit = self.emit
+        self.current_name = None
+
+    def emit(self, record):
+        self.write(self.log_handler.format(record))
+
+    def write(self, line):
+        self.writer(
+            f"{datetime.datetime.now(datetime.UTC):%Y-%m-%dT%H:%M:%S} "
+            f"[{self.current_name.rjust(self.max_name_width)}] "
+            f"{line}"
+        )
+
+    def capture_logs_as(self, name):
+        self.current_name = name
+        return self
+
+    def __enter__(self):
+        self.logger.setLevel("DEBUG")
+        self.logger.addHandler(self.log_handler)
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.logger.removeHandler(self.log_handler)
--- tests/data/management/commands/test_fetch.py
+++ tests/data/management/commands/test_fetch.py
@@ -1,3 +1,5 @@
+import io
+import logging
 from unittest.mock import Mock
 
 from django.conf import settings
@@ -40,3 +42,32 @@ def test_fetch_all(monkeypatch):
 
     mock_fetcher_1.assert_called_once()
     mock_fetcher_2.assert_called_once()
+
+
+def test_fetch_logging(monkeypatch, freezer):
+    log = logging.getLogger("openprescribing.data.fetchers")
+
+    def fetcher(*_, **__):
+        log.info("hello")
+        log.info("world")
+
+    def fetcher_2(*_, **__):
+        log.info("another")
+
+    monkeypatch.setattr(
+        fetch.Command,
+        "available_fetchers",
+        {
+            "fetcher": fetcher,
+            "fetcher_2": fetcher_2,
+        },
+    )
+
+    freezer.move_to("2025-01-02T03:04:05")
+    stdout = io.StringIO()
+    call_command("fetch", ["all"], stdout=stdout)
+    assert stdout.getvalue() == (
+        "2025-01-02T03:04:05 [  fetcher] hello\n"
+        "2025-01-02T03:04:05 [  fetcher] world\n"
+        "2025-01-02T03:04:05 [fetcher_2] another\n"
+    )
