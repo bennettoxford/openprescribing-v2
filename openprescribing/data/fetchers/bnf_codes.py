220b66da289c21191c017c984f3d5cb8d347d633
David Evans <dave.evans@thedatalab.org>
2025-11-11 15:24:19 +0000

    Add BNF fetcher

---
+++ openprescribing/data/fetchers/bnf_codes.py
@@ -0,0 +1,95 @@
+import datetime
+import logging
+import re
+import tempfile
+from pathlib import Path
+
+from openprescribing.data.utils.csv_to_parquet import csv_to_parquet
+from openprescribing.data.utils.filename_utils import (
+    get_latest_files_by_date,
+    get_temp_filename_for,
+)
+from openprescribing.data.utils.http_session import HTTPSession
+
+
+log = logging.getLogger(__name__)
+
+
+def fetch(directory):
+    dataset_dir = directory / "bnf_codes"
+    existing_files = dataset_dir.glob("*")
+
+    http = HTTPSession(
+        "https://opendata.nhsbsa.net/api/3/",
+        # We start with DEBUG level logging while we're checking if there's anything new
+        # to fetch
+        log=log.debug,
+    )
+    response = http.get(
+        "action/package_show",
+        params={"id": "bnf-code-information-current-year"},
+    )
+    response_data = response.json()
+
+    items_to_fetch = get_items_to_fetch(existing_files, response_data)
+
+    # Any requests we now make will be to fetch new files so we log at INFO level
+    http.log = log.info
+
+    for url, output_filename in items_to_fetch:
+        print(output_filename)
+        remote_csv_to_parquet(
+            http, url, dataset_dir / output_filename, encoding="latin-1"
+        )
+        log.info(f"Saved as: {output_filename}")
+
+
+def get_items_to_fetch(existing_files, response_data):
+    files_by_date = get_latest_files_by_date(existing_files)
+    resources = response_data["result"]["resources"]
+
+    to_fetch = []
+    already_fetched = 0
+    total = 0
+    for item in sorted(resources, key=lambda i: i["name"]):
+        match = re.match(
+            r"^BNF_CODE_CURRENT_(\d{6})_VERSION_(\d+)(_FINAL)?$", item["name"]
+        )
+        year_month = match.group(1)
+        version = int(match.group(2))
+        date = datetime.date.fromisoformat(f"{year_month}01")
+        published_at = datetime.datetime.fromisoformat(
+            # Oddly `last_modified` is sometimes null (maybe before the first update?)
+            # so we have to use the `created` date
+            item["last_modified"] or item["created"]
+        )
+
+        filename = (
+            f"bnf_codes_{date}_v{version:04}_{published_at:%Y-%m-%dT%H%M}.parquet"
+        )
+
+        if date in files_by_date and files_by_date[date].name >= filename:
+            already_fetched += 1
+        else:
+            to_fetch.append((item["url"], filename))
+
+        total += 1
+
+    # Use INFO where there are new files to fetch, DEBUG otherwise
+    (log.info if to_fetch else log.debug)(
+        f"Found {total} files: {already_fetched} already fetched and "
+        f"{len(to_fetch)} to fetch"
+    )
+
+    return to_fetch
+
+
+def remote_csv_to_parquet(http, csv_url, output_filename, **parquet_kwargs):
+    with tempfile.TemporaryDirectory() as tmp_name:
+        csv_path = Path(tmp_name) / "file.csv"
+        http.download_to_file(csv_url, csv_path)
+        # Convert extracted CSV to Parquet
+        output_filename.parent.mkdir(parents=True, exist_ok=True)
+        parquet_tmp = get_temp_filename_for(output_filename)
+        csv_to_parquet(csv_path, parquet_tmp, **parquet_kwargs)
+        parquet_tmp.replace(output_filename)
