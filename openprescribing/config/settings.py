d62568f14cbfcc55405f5b865d54b042d245e680
David Evans <dave.evans@thedatalab.org>
2025-11-14 12:38:47 +0000

    Add WhiteNoise for serving static assets

--- openprescribing/config/settings.py
+++ openprescribing/config/settings.py
@@ -73,6 +73,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -204,6 +205,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+# Setting this lets us avoid having to run `collectstatic` for now
+WHITENOISE_USE_FINDERS = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
