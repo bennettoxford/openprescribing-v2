086ec0ed6e762c5632d6e672311ec3d98a3891ee
Peter Inglesby <peter.inglesby@gmail.com>
2025-11-14 09:14:28 +0000

    Serve chart data from API

--- openprescribing/web/urls.py
+++ openprescribing/web/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from . import views
+from . import api, views
 
 
 urlpatterns = [
     path("", views.index),
+    path("api/prescribing/", api.prescribing, name="api_prescribing"),
 ]
--- openprescribing/web/views.py
+++ openprescribing/web/views.py
@@ -1,47 +1,12 @@
-import altair as alt
 from django.shortcuts import render
+from django.urls import reverse
 
-from openprescribing.data import rxdb
 from openprescribing.data.models import BNFCode
 
 
 def index(request):
     code = request.GET.get("code")
     bnf_code = BNFCode.objects.get(code=code)
-
-    sql = f"""
-    SELECT practice_id, date_id, items AS value
-    FROM prescribing
-    WHERE bnf_code LIKE '{bnf_code.code}%'
-    """
-
-    with rxdb.get_cursor() as cursor:
-        pdm = rxdb.get_practice_date_matrix(cursor, sql)
-
-    dates = pdm.col_labels
-    values = pdm.values.sum(axis=0)
-
-    chart_data = [
-        {"month": date.isoformat(), "items": value}
-        for date, value in zip(dates, values)
-    ]
-
-    chart_data.sort(key=lambda row: row["month"])
-
-    chart = (
-        alt.Chart(data=alt.Data(values=chart_data))
-        .mark_line(point=True)
-        .encode(
-            x=alt.X("month:T", title="Month"),
-            y=alt.Y("items:Q", title="Items"),
-            tooltip=[
-                alt.Tooltip("month:T", title="Month", format="%b %Y"),
-                alt.Tooltip("items:Q", title="Items", format=","),
-            ],
-        )
-        .properties(width=1200, height=360)
-    )
-
-    ctx = {"bnf_code": bnf_code, "chart_spec": chart.to_json()}
-
+    api_url = f"{reverse('api_prescribing')}?code={code}"
+    ctx = {"bnf_code": bnf_code, "prescribing_api_url": api_url}
     return render(request, "index.html", ctx)
