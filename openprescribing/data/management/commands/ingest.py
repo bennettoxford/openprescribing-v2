cc2cd95a389a21ce59b8cfad55ab9472f267dcc3
David Evans <dave.evans@thedatalab.org>
2025-11-14 10:24:20 +0000

    Issue WAL checkpoint whenever we ingest data
    This makes our caching logic work, but it's also independently sensible
    given our pattern of having a single, infrequent writer and many
    frequent reads: it's faster to query data in the main file rather than
    also having to consult the WAL.

--- openprescribing/data/management/commands/ingest.py
+++ openprescribing/data/management/commands/ingest.py
@@ -1,3 +1,4 @@
+import django.db
 from django.core.management.base import BaseCommand
 from django.db.utils import OperationalError
 
@@ -66,3 +67,31 @@ class Command(BaseCommand):
             ingestor = self.available_ingestors[name]
             with log_handler.capture_logs_as(name):
                 ingestor(force=force)
+                ensure_main_database_file_is_updated()
+
+
+def ensure_main_database_file_is_updated():
+    # Not every write to a SQLite database automatically causes the `.sqlite` file to be
+    # updated. When running in WAL mode, writes go to the `.wal` file in the first
+    # instance and only when this gets to a certain size do they get persisted in the
+    # main `.sqlite` file. This helps SQLite support large numbers of writes from
+    # multiple writers. However, we have only a single, infrequent writer and there are
+    # benefits to ensuring that all writes get immediately persisted in the `.sqlite`
+    # file:
+    #
+    #  1. The last-modified timestamp on the database file becomes an accurate
+    #     indication of when any data last changed, which is useful for caching
+    #     purposes.
+    #
+    #  2. Copying the single SQLite file always results in a full up-to-date copy of the
+    #     current production data.
+    #
+    # If there are no new commits in the WAL then this is a no-op and the last-modified
+    # timestamp remains unchanged.
+    conn = django.db.connections["data"]
+    with conn.cursor() as cursor:
+        # We use FULL (which ensures that the process completes) rather than default
+        # PASSIVE (which is best effort only). This blocks other writers but not other
+        # readers, which is fine for our purposes as `ingest` is the only writer.
+        # https://sqlite.org/pragma.html#pragma_wal_checkpoint
+        cursor.execute("PRAGMA wal_checkpoint(FULL)")
--- tests/data/management/commands/test_ingest.py
+++ tests/data/management/commands/test_ingest.py
@@ -2,9 +2,14 @@ import io
 import logging
 from unittest.mock import Mock
 
+import django.db
+import pytest
 from django.core.management import call_command
 
 from openprescribing.data.management.commands import ingest
+
+
+pytestmark = pytest.mark.django_db(databases=["data"])
 
 
 def test_ingest_named_ingestor(monkeypatch):
@@ -102,3 +107,35 @@ def test_ingest_logging_quiet(monkeypatch, freezer):
     stdout = io.StringIO()
     call_command("ingest", ["ingestor", "--quiet"], stdout=stdout)
     assert stdout.getvalue() == "2025-01-02T03:04:05 [ingestor] but this should\n"
+
+
+def test_ingest_ensures_main_database_file_is_updated(tmp_path, monkeypatch, settings):
+    # Create a test database which exists as a file on disk (the default test database
+    # is in-memory which is no good for our purposes here)
+    sqlite_path = tmp_path / "data.sqlite"
+    settings.DATABASES["data"]["NAME"] = sqlite_path
+    monkeypatch.delattr(django.db.connections._connections, "data")
+
+    with django.db.connections["data"].cursor() as cursor:
+        cursor.execute("CREATE TABLE t (v INT)")
+
+    initial_mtime = sqlite_path.stat().st_mtime
+
+    def ingestor(*_, **__):
+        with django.db.connections["data"].cursor() as cursor:
+            cursor.execute("INSERT INTO t VALUES (1), (2)")
+
+    # Run the ingestor directly and confirm that while it has inserted data the file
+    # modification timestamp has not changed
+    ingestor()
+    with django.db.connections["data"].cursor() as cursor:
+        assert cursor.execute("SELECT v FROM t").fetchall() == [(1,), (2,)]
+    assert sqlite_path.stat().st_mtime == initial_mtime
+
+    # Run the ingestor via the management command and confirm that it has now updated
+    # the timestamp
+    monkeypatch.setattr(ingest.Command, "available_ingestors", {"ingestor": ingestor})
+    call_command("ingest", ["ingestor"])
+    with django.db.connections["data"].cursor() as cursor:
+        assert cursor.execute("SELECT v FROM t").fetchall() == [(1,), (2,), (1,), (2,)]
+    assert sqlite_path.stat().st_mtime > initial_mtime
