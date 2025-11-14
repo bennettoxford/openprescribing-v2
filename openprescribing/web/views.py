43958233fa29cda88c5a0899c4bb1d7b4cf6f2c5
Peter Inglesby <peter.inglesby@gmail.com>
2025-11-14 09:14:28 +0000

    Add table showing monthly prescribing of Semaglutide

--- openprescribing/web/views.py
+++ openprescribing/web/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
 
+from openprescribing.data import rxdb
+
 
 def index(request):
-    return render(request, "index.html", {})
+    bnf_code = "0601023AW"  # Semaglutide
+
+    sql = f"""
+    SELECT practice_id, date_id, items AS value
+    FROM prescribing
+    WHERE bnf_code LIKE '{bnf_code}%'
+    """
+
+    with rxdb.get_cursor() as cursor:
+        pdm = rxdb.get_practice_date_matrix(cursor, sql)
+
+    labels = pdm.col_labels
+    values = pdm.values.sum(axis=0)
+
+    table = zip(labels, values)
+
+    ctx = {"table": table}
+
+    return render(request, "index.html", ctx)
