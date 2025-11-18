7f4c0d6e9c24c71153fe9fc32589bad66efb749b
David Evans <dave.evans@thedatalab.org>
2025-11-18 10:08:58 +0000

    Add an `Org` model to represent organisations

--- openprescribing/data/models/__init__.py
+++ openprescribing/data/models/__init__.py
@@ -1,8 +1,10 @@
 from .bnf_codes import BNFCode
 from .core import IngestedFile
+from .org import Org
 
 
 __all__ = [
     "BNFCode",
+    "Org",
     "IngestedFile",
 ]
---
+++ openprescribing/data/models/org.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+
+class Org(models.Model):
+    class Meta:
+        db_table = "org"
+
+    class OrgType(models.TextChoices):
+        REGION = ("reg", "Region")
+        ICB = ("icb", "ICB")
+        SICBL = ("sic", "SICBL")
+        PCN = ("pcn", "PCN")
+        PRACTICE = ("pra", "Practice")
+        OTHER = ("oth", "Other")
+
+    id = models.TextField(primary_key=True)
+    org_type = models.TextField(choices=OrgType, db_index=True)
+    name = models.TextField()
+    inactive = models.BooleanField(default=False)
+
+    parents = models.ManyToManyField(
+        "self",
+        related_name="children",
+        through="OrgRelation",
+        through_fields=("child", "parent"),
+        symmetrical=False,
+    )
+
+    def __repr__(self):
+        return f"Org({self.id!r}, {self.OrgType(self.org_type)!r}, {self.name!r})"
+
+
+class OrgRelation(models.Model):
+    class Meta:
+        db_table = "org_relation"
+
+    child = models.ForeignKey(
+        Org,
+        on_delete=models.CASCADE,
+        related_name="+",
+    )
+    parent = models.ForeignKey(
+        Org,
+        on_delete=models.CASCADE,
+        related_name="+",
+    )
