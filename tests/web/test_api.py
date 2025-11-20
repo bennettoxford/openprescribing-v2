c6c667b67c9fd92ac2bc2c6cef2756a8de687a6b
David Evans <dave.evans@thedatalab.org>
2025-11-20 10:47:22 +0000

    Update `api` module to use new method

--- tests/web/test_api.py
+++ tests/web/test_api.py
@@ -15,3 +15,17 @@ def test_prescribing(client, rxdb):
 
     rsp = client.get("/api/prescribing/?code=0601023AW")
     assert rsp.status_code == 200
+
+
+@pytest.mark.django_db(databases=["data"])
+def test_prescribing_with_practice(client, rxdb):
+    BNFCode.objects.create(code="0601023AW", name="Semaglutide", level=5)
+    Org.objects.create(id="PRAC05", name="Practice 5", org_type=Org.OrgType.PRACTICE)
+    rxdb.ingest(
+        [
+            {"bnf_code": "0601023AWAAAEAE", "practice_code": "PRAC05", "items": 10},
+        ]
+    )
+
+    rsp = client.get("/api/prescribing/?code=0601023AW&practice_id=PRAC05")
+    assert rsp.status_code == 200
