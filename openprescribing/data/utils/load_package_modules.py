dc0922aa7f0d35d649f054854d899755667d1192
Tom Ward <tomward@fmail.co.uk>
2026-01-09 10:34:06 +0000

    helpers -> functions

--- openprescribing/data/utils/load_package_modules.py
+++ openprescribing/data/utils/load_package_modules.py
@@ -3,8 +3,8 @@ import pkgutil
 
 
 def load_all_modules_with_function(path, name, function_name):
-    helpers = {}
+    functions = {}
     for m in pkgutil.walk_packages(path=path):
         mod = importlib.import_module(name + "." + m.name)
-        helpers[m.name] = getattr(mod, function_name)
-    return helpers
+        functions[m.name] = getattr(mod, function_name)
+    return functions
