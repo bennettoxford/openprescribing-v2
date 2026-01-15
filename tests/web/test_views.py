684272df8c6eca0c7a07f50a5cffdaccc085a6d0
Tom Ward <tomward@fmail.co.uk>
2026-01-15 13:56:26 +0000

    Move existing BNF code search to a new url

--- tests/web/test_views.py
+++ tests/web/test_views.py
@@ -3,16 +3,24 @@ import pytest
 from openprescribing.data.models import BNFCode, Org
 
 
-@pytest.mark.django_db(databases=["data"])
 def test_index(client):
+    rsp = client.get("/")
+    assert rsp.status_code == 200
+    assert "Prescribing data over time for a single BNF Code" in rsp.content.decode(
+        "utf-8"
+    )
+
+
+@pytest.mark.django_db(databases=["data"])
+def test_bnf_search(client):
     BNFCode.objects.create(code="0601023AW", name="Semaglutide", level=5)
     Org.objects.create(id="PRAC05", name="Practice 5", org_type=Org.OrgType.PRACTICE)
 
-    rsp = client.get("/")
+    rsp = client.get("/bnf_code/")
     assert rsp.status_code == 200
 
-    rsp = client.get("/?code=0601023AW")
+    rsp = client.get("/bnf_code/?code=0601023AW")
     assert rsp.status_code == 200
 
-    rsp = client.get("/?code=0601023AW&org_id=PRAC05")
+    rsp = client.get("/bnf_code/?code=0601023AW&org_id=PRAC05")
     assert rsp.status_code == 200
