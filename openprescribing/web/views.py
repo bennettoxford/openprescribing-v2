94f447e92fccc4d4928e7a19735eccea180633ba
Tom Ward <tomward@fmail.co.uk>
2026-01-15 14:37:20 +0000

    Duplicate bnf_code to bnf_codes

--- openprescribing/web/views.py
+++ openprescribing/web/views.py
@@ -43,3 +43,40 @@ def bnf_code(request):
     }
 
     return render(request, "bnf_code.html", ctx)
+
+
+def bnf_codes(request):
+    code = request.GET.get("code")
+    org_id = request.GET.get("org_id")
+
+    bnf_code = None
+    org = None
+    api_url = None
+
+    if org_id:
+        org = get_object_or_404(Org, id=org_id)
+
+    if code:
+        bnf_code = get_object_or_404(BNFCode, code=code)
+        api_url = f"{reverse('api_prescribing_deciles')}?code={code}"
+        if org_id:
+            api_url += f"&org_id={org_id}"
+
+    bnf_codes = list(BNFCode.objects.order_by("level", "name").values())
+    org_type_levels = [c[0] for c in Org.OrgType.choices]
+    orgs = sorted(
+        Org.objects.order_by("name").values("id", "name", "org_type"),
+        key=lambda o: org_type_levels.index(o["org_type"]),
+    )
+
+    ctx = {
+        "bnf_code": bnf_code,
+        "bnf_codes": bnf_codes,
+        "bnf_levels": BNFCode.Level.choices,
+        "org": org,
+        "orgs": orgs,
+        "org_types": Org.OrgType.choices,
+        "prescribing_api_url": api_url,
+    }
+
+    return render(request, "bnf_codes.html", ctx)
--- tests/web/test_views.py
+++ tests/web/test_views.py
@@ -19,3 +19,18 @@ def test_bnf_search(client, sample_data):
 
     rsp = client.get("/bnf_code/?code=1001030U0&org_id=PRA00")
     assert rsp.status_code == 200
+
+
+@pytest.mark.django_db(databases=["data"])
+def test_multiple_bnf_search(client):
+    BNFCode.objects.create(code="0601023AW", name="Semaglutide", level=5)
+    Org.objects.create(id="PRAC05", name="Practice 5", org_type=Org.OrgType.PRACTICE)
+
+    rsp = client.get("/bnf_codes/")
+    assert rsp.status_code == 200
+
+    rsp = client.get("/bnf_codes/?code=0601023AW")
+    assert rsp.status_code == 200
+
+    rsp = client.get("/bnf_codes/?code=0601023AW&org_id=PRAC05")
+    assert rsp.status_code == 200
