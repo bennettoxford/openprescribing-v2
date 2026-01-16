59c634cc0f647d18697e9a7bb6533b35837c8e68
Peter Inglesby <peter.inglesby@gmail.com>
2026-01-16 15:13:39 +0000

    Support searches by strength and formulation

--- openprescribing/data/rxdb/search.py
+++ openprescribing/data/rxdb/search.py
@@ -8,12 +8,18 @@ from ..models import BNFCode
 def search(query):
     """Return BNF codes (as strings) of presentations matching query.
 
-    A query is a list containing BNF codes at any level of the BNF hierarchy (again, as
-    strings), which are optionally excluded with a leading "-".
+    A query is a list of terms, where a term is either:
+
+      * a BNF code (as a string) at any level of the hierarchy, which matches all
+        presentations below that code;
+      * a chemical's BNF code, followed by an underscore, followed by the two-character
+        identifier for a strength and formulation (such as 040702040_AM for Tramadol HCl
+        300mg tablets), which matches all presentations belonging to the chemical with
+        the given strength and formulation.
     """
 
-    includes = [Q(code__startswith=term) for term in query if term[0] != "-"]
-    excludes = [Q(code__startswith=term[1:]) for term in query if term[0] == "-"]
+    includes = [_build_q(term) for term in query if term[0] != "-"]
+    excludes = [_build_q(term[1:]) for term in query if term[0] == "-"]
 
     results = (
         BNFCode.objects.filter(level=BNFCode.Level.PRESENTATION)
@@ -24,3 +30,14 @@ def search(query):
     )
 
     return list(results)
+
+
+def _build_q(term):
+    if "_" in term:
+        assert term.count("_") == 1
+        prefix, suffix = term.split("_")
+        assert len(prefix) == 9  # chemical code
+        assert len(suffix) == 2  # strength and formulation
+        return Q(code__startswith=prefix, code__endswith=suffix)
+    else:
+        return Q(code__startswith=term)
--- tests/data/rxdb/test_search.py
+++ tests/data/rxdb/test_search.py
@@ -15,3 +15,8 @@ def test_search(bnf_codes):
 @pytest.mark.django_db(databases=["data"])
 def test_search_no_excludes(bnf_codes):
     assert search(["1001030U0AA"]) == ["1001030U0AAABAB", "1001030U0AAACAC"]
+
+
+@pytest.mark.django_db(databases=["data"])
+def test_search_for_strength_and_formulation(bnf_codes):
+    assert search(["1001030U0_AC"]) == ["1001030U0AAACAC", "1001030U0BDABAC"]
