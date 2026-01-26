5d20ccee7cbe89fe6b3058dbe9780d1f182fa2a6
Peter Inglesby <peter.inglesby@gmail.com>
2026-01-26 09:51:19 +0000

    Add methods to BNFCode to make it easier to work with BNF structure

--- openprescribing/data/models/bnf_codes.py
+++ openprescribing/data/models/bnf_codes.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 from django.db import models
 
 
@@ -17,3 +19,46 @@ class BNFCode(models.Model):
     code = models.TextField(primary_key=True)
     level = models.IntegerField(choices=Level)
     name = models.TextField()
+
+    @property
+    def slots(self):
+        boundaries = [0, 2, 4, 6, 7, 9, 11, 13, 15]
+        kwargs = {}
+        for ix, field in enumerate(Slots.__dataclass_fields__):
+            if len(self.code) >= boundaries[ix + 1]:
+                kwargs[field] = self.code[boundaries[ix] : boundaries[ix + 1]]
+            else:
+                kwargs[field] = None
+        return Slots(**kwargs)
+
+    def is_generic(self):
+        assert self.level in [BNFCode.Level.PRODUCT, BNFCode.Level.PRESENTATION]
+        return self.slots.product == "AA"
+
+    def is_ancestor_of(self, other):
+        return other.code != self.code and other.code.startswith(self.code)
+
+    def is_generic_equivalent_of(self, other):
+        assert self.level == BNFCode.Level.PRESENTATION
+        assert other.level == BNFCode.Level.PRESENTATION
+        assert self.is_generic()
+        return (
+            self.slots.chapter == other.slots.chapter
+            and self.slots.section == other.slots.section
+            and self.slots.paragraph == other.slots.paragraph
+            and self.slots.subparagraph == other.slots.subparagraph
+            and self.slots.chemical_substance == other.slots.chemical_substance
+            and self.slots.strength_and_formulation == other.slots.generic_equivalent
+        )
+
+
+@dataclass
+class Slots:
+    chapter: str | None
+    section: str | None
+    paragraph: str | None
+    subparagraph: str | None
+    chemical_substance: str | None
+    product: str | None
+    strength_and_formulation: str | None
+    generic_equivalent: str | None
---
+++ tests/data/models/test_bnf_codes.py
@@ -0,0 +1,60 @@
+import pytest
+
+from openprescribing.data.models import BNFCode
+from openprescribing.data.models.bnf_codes import Slots
+
+
+@pytest.mark.django_db(databases=["data"])
+def test_slots(bnf_codes):
+    assert bnf_code("10").slots == Slots(
+        chapter="10",
+        section=None,
+        paragraph=None,
+        subparagraph=None,
+        chemical_substance=None,
+        product=None,
+        strength_and_formulation=None,
+        generic_equivalent=None,
+    )
+
+    assert bnf_code("1001030U0BDABAC").slots == Slots(
+        chapter="10",
+        section="01",
+        paragraph="03",
+        subparagraph="0",
+        chemical_substance="U0",
+        product="BD",
+        strength_and_formulation="AB",
+        generic_equivalent="AC",
+    )
+
+
+@pytest.mark.django_db(databases=["data"])
+def test_is_generic(bnf_codes):
+    assert bnf_code("1001030U0AA").is_generic()
+    assert not bnf_code("1001030U0BD").is_generic()
+    assert bnf_code("1001030U0AAABAB").is_generic()
+    assert not bnf_code("1001030U0BDAAAB").is_generic()
+
+
+@pytest.mark.django_db(databases=["data"])
+def test_is_ancestor_of(bnf_codes):
+    assert bnf_code("1001").is_ancestor_of(bnf_code("100103"))
+    assert bnf_code("1001030U0").is_ancestor_of(bnf_code("1001030U0AA"))
+    assert bnf_code("1001030U0AA").is_ancestor_of(bnf_code("1001030U0AAABAB"))
+    assert not bnf_code("1001030U0AA").is_ancestor_of(bnf_code("1001030U0BDAAAB"))
+    assert not bnf_code("1001030U0AA").is_ancestor_of(bnf_code("1001030U0AA"))
+
+
+@pytest.mark.django_db(databases=["data"])
+def test_is_generic_equivalent_of(bnf_codes):
+    assert bnf_code("1001030U0AAABAB").is_generic_equivalent_of(
+        bnf_code("1001030U0BDAAAB")
+    )
+    assert not bnf_code("1001030U0AAABAB").is_generic_equivalent_of(
+        bnf_code("1001030U0BDABAC")
+    )
+
+
+def bnf_code(code):
+    return BNFCode.objects.get(code=code)
