756d9218a1524416cbf6582073abebfffd72e157
Peter Inglesby <peter.inglesby@gmail.com>
2026-01-28 09:59:09 +0000

    Update functional test to check contents of modal

--- tests/functional/test_bnf_browser.py
+++ tests/functional/test_bnf_browser.py
@@ -9,8 +9,10 @@ pytestmark = pytest.mark.functional
 
 @pytest.mark.django_db(databases=["data"])
 def test_bnf_browser(live_server, page, sample_data):
-    page.goto(live_server.url)
-    page.get_by_role("link", name="BNF browser").click()
+    page.goto(live_server.url + "/bnf/")
+
+    modal = page.locator("#bnf-modal")
+    modal_body = page.locator("#bnf-modal-body")
 
     # Test that clicking on nodes expands them.
     expect(page.get_by_text("Drugs used in diabetes")).not_to_be_visible()
@@ -27,13 +29,14 @@ def test_bnf_browser(live_server, page, sample_data):
 
     # Test that the codes for a chemical substance (without generics) and its products
     # and presentations are visible.
-    with page.expect_popup() as glucose_blood_popup:
-        page.get_by_text("0601060D0 Glucose blood").click()
-    glucose_blood_page = glucose_blood_popup.value
-    for bnf_code in BNFCode.objects.filter(code__startswith="0601060D0"):
-        expect(
-            glucose_blood_page.get_by_text(bnf_code.code, exact=True)
-        ).to_be_visible()
+    page.get_by_text("0601060D0 Glucose blood").click()
+    expect(modal).to_be_visible()
+    for bnf_code in BNFCode.objects.filter(code__startswith="0601060D0").exclude(
+        code="0601060D0"
+    ):
+        expect(modal_body.get_by_text(bnf_code.code, exact=True)).to_be_visible()
+    modal.get_by_role("button").click()
+    expect(modal).not_to_be_visible()
 
     # Test that clicking on an expanded node hides its descendants.
     expect(page.get_by_text("Drugs used in diabetes")).to_be_visible()
@@ -55,8 +58,11 @@ def test_bnf_browser(live_server, page, sample_data):
 
     # Test that the codes for the chemical substance and its products and presentations
     # are visible.
-    with page.expect_popup() as methotrexate_popup:
-        page.get_by_text("1001030U0 Methotrexate").click()
-    methotrexate_page = methotrexate_popup.value
-    for bnf_code in BNFCode.objects.filter(code__startswith="1001030U0"):
-        expect(methotrexate_page.get_by_text(bnf_code.code, exact=True)).to_be_visible()
+    page.get_by_text("1001030U0 Methotrexate").click()
+    expect(modal).to_be_visible()
+    for bnf_code in BNFCode.objects.filter(code__startswith="1001030U0").exclude(
+        code="1001030U0"
+    ):
+        expect(modal_body.get_by_text(bnf_code.code, exact=True)).to_be_visible()
+    modal.get_by_role("button").click()
+    expect(modal).not_to_be_visible()
