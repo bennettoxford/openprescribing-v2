0f31103cbaf91fede4b9d4d588814c34713109db
Peter Inglesby <peter.inglesby@gmail.com>
2026-02-02 16:58:40 +0000

    Update test to demonstrate new functionality
    This is only really a smoke test.  It might be better to test frontend
    code via JS tests rather than by Playwright.

--- tests/functional/test_bnf_codes.py
+++ tests/functional/test_bnf_codes.py
@@ -18,15 +18,19 @@ def test_bnf_codes(live_server, page: Page, sample_data):
     # Wait for the JS to load, just in case
     page.wait_for_load_state("networkidle")
 
-    numerator_codes = page.get_by_placeholder("BNF codes for numerator")
-    numerator_codes.click()
-    numerator_codes.fill("1001030U0AA\n-1001030U0AAACAC\n")
-
+    page.get_by_role("textbox", name="BNF codes for numerator").click()
+    page.get_by_text("10 Musculoskeletal and Joint Diseases").click()
+    page.get_by_text("1001 Drugs used in rheumatic").click()
+    page.get_by_text("100103 Rheumatic disease").click()
+    page.get_by_text("1001030 Rheumatic disease").click()
+    page.get_by_text("1001030 Rheumatic disease").click(modifiers=["Shift"])
+    page.get_by_text("1001030U0 Methotrexate").click(modifiers=["Shift"])
+    page.get_by_role("button", name="Update query").click()
     page.get_by_role("button", name="Submit").click()
 
     expect(page).to_have_url(
         live_server.url
-        + "/bnf_codes/?ntr_codes=1001030U0AA%0D%0A-1001030U0AAACAC%0D%0A&ntr_product_type=all&dtr_codes=&dtr_product_type=all"
+        + "/bnf_codes/?ntr_codes=1001030%0D%0A-1001030U0&ntr_product_type=all&dtr_codes=&dtr_product_type=all"
     )
 
     # Test if the org search works
@@ -36,5 +40,5 @@ def test_bnf_codes(live_server, page: Page, sample_data):
 
     expect(page).to_have_url(
         live_server.url
-        + "/bnf_codes/?ntr_codes=1001030U0AA%0D%0A-1001030U0AAACAC%0D%0A&ntr_product_type=all&dtr_codes=&dtr_product_type=all&org_id=ICB01"
+        + "/bnf_codes/?ntr_codes=1001030%0D%0A-1001030U0&ntr_product_type=all&dtr_codes=&dtr_product_type=all&org_id=ICB01"
     )
