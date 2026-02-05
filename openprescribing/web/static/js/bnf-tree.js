b33502043ad3274033b6fe7ae0376859c328f96e
Peter Inglesby <peter.inglesby@gmail.com>
2026-02-05 13:06:27 +0000

    Simplify how JS for BNF tree is set up
    I had made this change in #117, when I had thought we would be reusing
    the same JS code for both the BNF browser and the presentation picker.

--- openprescribing/web/static/js/bnf-tree.js
+++ openprescribing/web/static/js/bnf-tree.js
@@ -14,50 +14,47 @@
 // does not make sense to combine implementations.  When making changes here, consider
 // also making changes there.
 
-export function setUpBNFTree() {
-  const tree = document.getElementById("bnf-tree");
-  const searchForm = document.getElementById("bnf-search-form");
-  const modal = document.getElementById("bnf-table-modal");
+const tree = document.getElementById("bnf-tree");
+const searchForm = document.getElementById("bnf-search-form");
+const modal = document.getElementById("bnf-table-modal");
+const modalObj = new bootstrap.Modal(modal);
+const modalTitle = modal.querySelector(".modal-title");
+const modalBody = modal.querySelector(".modal-body");
 
-  const modalObj = new bootstrap.Modal(modal);
-  const modalTitle = modal.querySelector(".modal-title");
-  const modalBody = modal.querySelector(".modal-body");
+tree.addEventListener("click", (e) => {
+  const li = e.target.closest("li");
+  const code = li.dataset.code;
+  if (code.length === 9) {
+    modalTitle.innerHTML = `<code>${code}</code> ${li.dataset.name}`;
+    modalBody.innerHTML = `
+    <div class="text-center py-5">
+	<div class="spinner-border">
+	</div>
+    </div>
+`;
+    modalObj.show();
+    htmx.ajax("GET", `/bnf/${code}/`, {
+      target: "#bnf-table-modal .modal-body",
+      swap: "innerHTML",
+    });
+  } else {
+    li.toggleAttribute("data-open");
+  }
+});
 
-  tree.addEventListener("click", (e) => {
-    const li = e.target.closest("li");
-    const code = li.dataset.code;
-    if (code.length === 9) {
-      modalTitle.innerHTML = `<code>${code}</code> ${li.dataset.name}`;
-      modalBody.innerHTML = `
-            <div class="text-center py-5">
-                <div class="spinner-border">
-                </div>
-            </div>
-        `;
-      modalObj.show();
-      htmx.ajax("GET", `/bnf/${code}/`, {
-        target: "#bnf-table-modal .modal-body",
-        swap: "innerHTML",
-      });
+const searchInput = searchForm.querySelector("input");
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const needle = searchInput.value.trim().toLowerCase();
+  tree.querySelectorAll("li").forEach((li) => {
+    if (
+      li.dataset.code.toLowerCase() === needle ||
+      li.dataset.name.toLowerCase().includes(needle)
+    ) {
+      li.setAttribute("data-matches-search", "");
     } else {
-      li.toggleAttribute("data-open");
+      li.removeAttribute("data-matches-search");
     }
   });
-
-  const searchInput = searchForm.querySelector("input");
-
-  searchForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const needle = searchInput.value.trim().toLowerCase();
-    tree.querySelectorAll("li").forEach((li) => {
-      if (
-        li.dataset.code.toLowerCase() === needle ||
-        li.dataset.name.toLowerCase().includes(needle)
-      ) {
-        li.setAttribute("data-matches-search", "");
-      } else {
-        li.removeAttribute("data-matches-search");
-      }
-    });
-  });
-}
+});
