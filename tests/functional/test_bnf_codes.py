1065ee773f7d688b1485f66d9f10dd0eacc76542
Peter Inglesby <peter.inglesby@gmail.com>
2026-02-06 09:37:29 +0000

    Add smoke test for BNF picker

--- tests/functional/test_bnf_codes.py
+++ tests/functional/test_bnf_codes.py
@@ -7,29 +7,34 @@ pytestmark = pytest.mark.functional
 
 @pytest.mark.django_db(databases=["data"], transaction=True)
 def test_bnf_codes(live_server, page: Page, sample_data):
-    page.goto(live_server.url)
-    expect(page).to_have_url(live_server.url + "/")
-
-    page.get_by_role(
-        "link", name="Prescribing data over time for multiple BNF Codes"
-    ).click()
-    expect(page).to_have_url(live_server.url + "/bnf_codes/")
-
-    # Wait for the JS to load, just in case
-    page.wait_for_load_state("networkidle")
-
-    numerator_codes = page.get_by_placeholder("BNF codes for numerator")
-    numerator_codes.click()
-    numerator_codes.fill("1001030U0AA\n-1001030U0AAACAC\n")
-
+    # This is a limited smoke test that checks that codes can be selected from the table
+    # (numerator) and the tree (denominator), and that on form submission we're directed
+    # to the expected URL.
+    page.goto(live_server.url + "/bnf_codes/")
+    page.get_by_role("textbox", name="BNF codes for numerator").click()
+    page.get_by_role("textbox", name="Search by name or code").click()
+    page.get_by_role("textbox", name="Search by name or code").fill("metho")
+    page.get_by_role("button", name="Search").click()
+    page.get_by_text("1001030U0 Methotrexate").click()
+    page.get_by_text("1001030U0AAACAC").click(modifiers=["ControlOrMeta"])
+    page.locator("#bnf-table-modal").get_by_role("button", name="Update Query").click()
+    page.locator("#bnf-tree-modal").get_by_role("button", name="Update Query").click()
+    page.get_by_role("textbox", name="BNF codes for denominator").click()
+    page.get_by_role("textbox", name="Search by name or code").click()
+    page.get_by_role("textbox", name="Search by name or code").fill("metho")
+    page.get_by_role("button", name="Search").click()
+    page.locator("#bnf-tree-modal").get_by_text("1001030U0 Methotrexate").click(
+        modifiers=["ControlOrMeta"]
+    )
+    page.locator("#bnf-tree-modal").get_by_role("button", name="Update Query").click()
     page.get_by_role("button", name="Submit").click()
 
     expect(page).to_have_url(
         live_server.url
-        + "/bnf_codes/?ntr_codes=1001030U0AA%0D%0A-1001030U0AAACAC%0D%0A&ntr_product_type=all&dtr_codes=&dtr_product_type=all"
+        + "/bnf_codes/?ntr_codes=1001030U0_AC&ntr_product_type=all&dtr_codes=1001030U0&dtr_product_type=all"
     )
 
-    # Test if the org search works
+    # Test org search
     page.get_by_role(
         "searchbox", name="Name or code of organisation to highlight"
     ).click()
@@ -40,5 +45,5 @@ def test_bnf_codes(live_server, page: Page, sample_data):
 
     expect(page).to_have_url(
         live_server.url
-        + "/bnf_codes/?ntr_codes=1001030U0AA%0D%0A-1001030U0AAACAC%0D%0A&ntr_product_type=all&dtr_codes=&dtr_product_type=all&org_id=ICB01"
+        + "/bnf_codes/?ntr_codes=1001030U0_AC&ntr_product_type=all&dtr_codes=1001030U0&dtr_product_type=all&org_id=ICB01"
     )
