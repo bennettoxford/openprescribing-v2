d2532719981c40bc9184f2779eeeb654b31c0452
Tom Ward <tomward@fmail.co.uk>
2026-02-06 15:55:29 +0000

    Correctly remove descendant exclusions in tables

--- openprescribing/web/static/js/prescribing-query.js
+++ openprescribing/web/static/js/prescribing-query.js
@@ -272,6 +272,17 @@ function handleTreeCtrlClick(li) {
       removeItem(query.excluded, n.dataset.code);
       n.removeAttribute("data-excluded");
     });
+    // descendant exclusions which are part of the table aren't
+    // found by the li selector, so remove them directly
+    query.excluded.forEach((n) => {
+      if (isAncestor(code, n)) {
+        removeItem(query.excluded, n);
+      }
+    });
+    li.removeAttribute("data-partially-included");
+    li.querySelectorAll("li[data-partially-included]").forEach((n) => {
+      n.removeAttribute("data-partially-included");
+    });
   } else if (isDirectlyExcluded(query, code)) {
     // This item is directly excluded:
     // * Don't exclude it
