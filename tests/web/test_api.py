d7ddb0b1ae3f375d2ae79c0f6aa92fa1c6910f12
Iain Dillingham <iain@dillingham.me.uk>
2026-02-18 08:57:03 +0000

    Always test payload
    It wasn't clear to me why some tests were testing only the status code,
    but others were also testing the payload. Here, we do both. This
    highlights that testing with a practice changes the deciles from ICB
    deciles (the current default) to practice deciles. It's also important
    that the payload contains an org key, even when there is no highlight
    organisation, so we test for this too. (The deciles chart wouldn't
    render correctly without an org key.)

--- tests/web/test_api.py
+++ tests/web/test_api.py
@@ -6,21 +6,28 @@ from openprescribing.web import api
 @pytest.mark.django_db(databases=["data"])
 def test_prescribing_deciles(client, sample_data):
     rsp = client.get("/api/prescribing-deciles/?ntr_codes=1001030U0")
+    payload = rsp.json()
     assert rsp.status_code == 200
-    assert rsp.json()["deciles"][-1]["value"] == pytest.approx(64.15, 0.001)
+    assert payload["deciles"][-1]["value"] == pytest.approx(64.15, 0.001)
+    assert payload["org"] == []
 
 
 @pytest.mark.django_db(databases=["data"])
 def test_prescribing_deciles_with_practice(client, sample_data):
     rsp = client.get("/api/prescribing-deciles/?ntr_codes=1001030U0&org_id=PRA00")
+    payload = rsp.json()
     assert rsp.status_code == 200
+    assert payload["deciles"][-1]["value"] == pytest.approx(66.41, 0.001)
+    assert payload["org"][-1]["value"] == pytest.approx(51.94, 0.001)
 
 
 @pytest.mark.django_db(databases=["data"])
 def test_prescribing_deciles_with_exclusion(client, sample_data):
     rsp = client.get("/api/prescribing-deciles/?ntr_codes=1001030U0,-1001030U0AAABAB")
+    payload = rsp.json()
     assert rsp.status_code == 200
-    assert rsp.json()["deciles"][-1]["value"] == pytest.approx(59.07, 0.001)
+    assert payload["deciles"][-1]["value"] == pytest.approx(59.07, 0.001)
+    assert payload["org"] == []
 
 
 @pytest.mark.django_db(databases=["data"])
@@ -28,7 +35,10 @@ def test_prescribing_deciles_with_denominator(client, sample_data):
     rsp = client.get(
         "/api/prescribing-deciles/?ntr_codes=1001030U0AA&dtr_codes=1001030U0"
     )
+    payload = rsp.json()
     assert rsp.status_code == 200
+    assert payload["deciles"][-1]["value"] == pytest.approx(27.14, 0.001)
+    assert payload["org"] == []
 
 
 def test_nans_to_nones():
