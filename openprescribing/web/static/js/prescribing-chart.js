9035a0a453ab5ae50668c150026e2b37ce27180e
Peter Inglesby <peter.inglesby@gmail.com>
2026-03-04 11:04:49 +0000

    Remove unnecessary guard
    If prescribingDecilesUrl is set, then prescribingAllOrgsUrl is also set.

--- openprescribing/web/static/js/prescribing-chart.js
+++ openprescribing/web/static/js/prescribing-chart.js
@@ -161,31 +161,29 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
 
-    if (prescribingAllOrgsUrl) {
-      document
-        .getElementById("all_orgs_line_chart")
-        .addEventListener("click", () => {
-          if (prescribingAllOrgsUrl) {
-            updateDecilesChart(
-              prescribingAllOrgsUrl,
-              "all_orgs",
-              "all_orgs_line_chart",
-            );
-          }
-        });
+    document
+      .getElementById("all_orgs_line_chart")
+      .addEventListener("click", () => {
+        if (prescribingAllOrgsUrl) {
+          updateDecilesChart(
+            prescribingAllOrgsUrl,
+            "all_orgs",
+            "all_orgs_line_chart",
+          );
+        }
+      });
 
-      document
-        .getElementById("all_orgs_dots_chart")
-        .addEventListener("click", () => {
-          if (prescribingAllOrgsUrl) {
-            updateDecilesChart(
-              prescribingAllOrgsUrl,
-              "all_orgs",
-              "all_orgs_dots_chart",
-            );
-          }
-        });
-    }
+    document
+      .getElementById("all_orgs_dots_chart")
+      .addEventListener("click", () => {
+        if (prescribingAllOrgsUrl) {
+          updateDecilesChart(
+            prescribingAllOrgsUrl,
+            "all_orgs",
+            "all_orgs_dots_chart",
+          );
+        }
+      });
 
     // default to decile view!
     document.getElementById("decile").click();
