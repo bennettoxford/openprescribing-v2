bennettoxford/openprescribing-v2

commit 772d953ccd3eac2bb73aedfdeaebb9fee2e478c8

Add a radio button to switch between datasets in the chart
* loads the data from the server on every click
  * we will likely need to ensure the server is caching these results!

openprescribing/web/static/js/prescribing-chart.js
@@ -102,15 +102,23 @@ const createTypeahead = ({
   });
 };
 
-const updateDecilesChart = (prescribingDecilesUrl) => {
+var chartResult;
+
+const createDecilesChart = () => {
   const chartContainer = document.querySelector("#deciles-chart-container");
   const chartSpec = JSON.parse(
     document.getElementById("deciles-chart").textContent,
   );
 
   const opt = { renderer: "svg" };
-  const chartResult = vegaEmbed(chartContainer, chartSpec, opt);
+  chartResult = vegaEmbed(chartContainer, chartSpec, opt);
+};
 
+const updateDecilesChart = (
+  prescribingDecilesUrl,
+  add_dataset_name,
+  remove_dataset_name,
+) => {
   fetch(prescribingDecilesUrl)
     .then((response) => {
       if (!response.ok) {
@@ -119,17 +127,20 @@ const updateDecilesChart = (prescribingDecilesUrl) => {
       return response.json();
     })
     .then((response) => {
-      response.deciles.forEach((record) => {
+      response[add_dataset_name].forEach((record) => {
         record.month = new Date(record.month);
       });
-      response.org.forEach((record) => {
-        record.month = new Date(record.month);
-      });
+      if (response.org) {
+        response.org.forEach((record) => {
+          record.month = new Date(record.month);
+        });
+      }
       chartResult.then((result) => {
-        result.view
-          .insert("deciles", response.deciles)
-          .insert("org", response.org)
-          .run();
+        result.view.insert(add_dataset_name, response[add_dataset_name]);
+        if (response.org) {
+          result.view.insert("org", response.org);
+        }
+        result.view.remove(remove_dataset_name, () => true).run();
       });
     })
     .catch((error) => {
@@ -150,7 +161,28 @@ document.addEventListener("DOMContentLoaded", () => {
   const prescribingDecilesUrl = JSON.parse(
     document.getElementById("prescribing-deciles-url").textContent,
   );
+  const prescribingAllOrgsUrl = JSON.parse(
+    document.getElementById("prescribing-all-orgs-url").textContent,
+  );
+
   if (prescribingDecilesUrl) {
-    updateDecilesChart(prescribingDecilesUrl);
+    createDecilesChart();
+
+    document.getElementById("decile").addEventListener("click", () => {
+      if (prescribingDecilesUrl) {
+        updateDecilesChart(prescribingDecilesUrl, "deciles", "all_orgs");
+      }
+    });
+
+    if (prescribingAllOrgsUrl) {
+      document.getElementById("all_orgs").addEventListener("click", () => {
+        if (prescribingAllOrgsUrl) {
+          updateDecilesChart(prescribingAllOrgsUrl, "all_orgs", "deciles");
+        }
+      });
+    }
+
+    // default to decile view!
+    document.getElementById("decile").click();
   }
 });

openprescribing/web/templates/query.html
@@ -46,6 +46,15 @@
                 {% if ntr_codes %}
                 <h1 class="h4">Prescribing data</h1>
                 <div id="deciles-chart-container" class="py-3">Loading chart...</div>
+                <div>
+                    <fieldset>
+                        <h2 class="h5">Show prescribing</h2>
+                        <input type="radio" id="decile" name="chart_type" value="decile" selected>
+                        <label for="decile">Deciles</label><br>
+                        <input type="radio" id="all_orgs" name="chart_type" value="all_orgs">
+                        <label for="all_orgs">All organisations</label><br>
+                    </fieldset>
+                </div>
                 <div class="row">
                     <div class="col-6">
                         <h2 class="h5">Numerator</h2>

commit 1d4445091c782b85ec4e9db4ddc6e985176890c1

Do not show empty chart whilst loading data
* fixes bug introduced in https://github.com/bennettoxford/openprescribing-v2/pull/169
* only create the chart when we have data available
* only create the chart if it hasn't already been created
* we have an idea about recreating the vega-embed every time
  we switch chart dataset/type, so this whole section might be refactored
  out of existence quite soon

openprescribing/web/static/js/prescribing-chart.js
@@ -89,8 +89,7 @@ const updateOrgTypeLabel = (orgType) => {
 
 var chartResult;
 
-const createDecilesChart = () => {
-  const chartContainer = document.querySelector("#deciles-chart-container");
+const createDecilesChart = (chartContainer) => {
   const chartSpec = JSON.parse(
     document.getElementById("deciles-chart").textContent,
   );
@@ -104,6 +103,11 @@ const updateDecilesChart = (
   api_dataset_name,
   add_dataset_name,
 ) => {
+  const chartContainer = document.querySelector("#deciles-chart-container");
+  if (!document.querySelector("#deciles-chart-container").classList.contains('vega-embed')) {
+    createDecilesChart(chartContainer);
+  }
+
   const all_dataset_names = ["deciles", "all_orgs_dots", "all_orgs_line"];
   fetch(prescribingDecilesUrl)
     .then((response) => {
@@ -155,8 +159,6 @@ document.addEventListener("DOMContentLoaded", () => {
   );
 
   if (prescribingDecilesUrl) {
-    createDecilesChart();
-
     document.getElementById("decile").addEventListener("click", () => {
       updateDecilesChart(prescribingDecilesUrl, "deciles", "deciles");
     });