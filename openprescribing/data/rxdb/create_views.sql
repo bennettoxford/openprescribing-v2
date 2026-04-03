c34f744b9e686374d9e65bfcadfcbe383ebc8b09
Peter Inglesby <peter.inglesby@gmail.com>
2026-04-03 10:41:00 +0100

    Create medications view in create_views.sql
    This view contains one row for each VMP and AMP, with enough metadata to
    allow us to query for medications by ingredient, VTM, and form_route.
    
    Over time we will expand the scope of the view to support querying by
    any useful dm+d field, as well as BNF codes once we have a dm+d -> BNF
    mapping.

--- openprescribing/data/rxdb/create_views.sql
+++ openprescribing/data/rxdb/create_views.sql
@@ -2,3 +2,46 @@
 -- that file, but views which involve tables in the SQLite database must be defined
 -- here. These are initialised dynamically when the RXDB connection is created so
 -- there's no need for any migrations.
+
+CREATE VIEW medications AS
+SELECT
+    id,
+    name,
+    is_amp,
+    vmp_id,
+    vtm_id,
+    cast(invalid AS boolean),
+    array(
+        SELECT ont.formcd
+        FROM ont
+        WHERE ont.vpid = vmp_id
+	ORDER BY ont.formcd
+    ) AS form_route_ids,
+    array(
+        SELECT vpi.isid
+        FROM vpi
+        WHERE vpi.vpid = vmp_id
+	ORDER BY vpi.vpid
+    ) AS ingredient_ids,
+FROM (
+    SELECT
+        vmp.vpid AS id,
+        vmp.nm AS name,
+        false AS is_amp,
+        vmp.vpid AS vmp_id,
+        vmp.vtmid AS vtm_id,
+	vmp.invalid AS invalid,
+    FROM vmp
+
+    UNION ALL
+
+    SELECT
+        amp.apid AS id,
+        amp.descr AS name,
+        true AS is_amp,
+        vmp.vpid AS vmp_id,
+        vmp.vtmid AS vtm_id,
+	amp.invalid AS invalid,
+    FROM amp
+    JOIN vmp ON amp.vpid = vmp.vpid
+);
