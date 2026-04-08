1243cff0f490609fc871bd8cf7cdc4c47f9ba325
Tom Ward <tomward@fmail.co.uk>
2026-04-08 16:48:56 +0100

    Build an Analysis from a dict

--- tests/data/test_analysis.py
+++ tests/data/test_analysis.py
@@ -58,3 +58,53 @@ def test_to_params_dtr_list_size():
         "ntr_codes": "01,-0101",
         "ntr_product_type": "branded",
     }
+
+
+def test_from_dict():
+    analysis_dict = {
+        "queries": [
+            {
+                "numerator": {
+                    "product_type": "branded",
+                    "bnf_codes": {
+                        "included": ["01"],
+                        "excluded": ["0101"],
+                    },
+                },
+                "denominator": {
+                    "bnf_codes": {
+                        "included": ["01"],
+                        "excluded": ["0101"],
+                    }
+                },
+            }
+        ]
+    }
+    analysis = Analysis.from_dict(analysis_dict)
+    assert analysis.to_params() == {
+        "ntr_codes": "01,-0101",
+        "ntr_product_type": "branded",
+        "dtr_codes": "01,-0101",
+        "dtr_product_type": "all",
+    }
+
+
+def test_from_dict_numerator_only():
+    analysis_dict = {
+        "queries": [
+            {
+                "numerator": {
+                    "product_type": "branded",
+                    "bnf_codes": {
+                        "included": ["01"],
+                        "excluded": ["0101"],
+                    },
+                }
+            }
+        ]
+    }
+    analysis = Analysis.from_dict(analysis_dict)
+    assert analysis.to_params() == {
+        "ntr_codes": "01,-0101",
+        "ntr_product_type": "branded",
+    }
