7adae630ae76a23423e2549ca17a9a22bc1f8f3e
Tom Ward <tomward@fmail.co.uk>
2026-04-08 16:49:53 +0100

    Add a convenience for loading YAML measures

---
+++ openprescribing/data/measures/__init__.py
@@ -0,0 +1,6 @@
+from .measures import load_measure
+
+
+__all__ = [
+    "load_measure",
+]
---
+++ openprescribing/data/measures/measures.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+import yaml
+
+
+def load_measure(measure_name):  # pragma: no cover
+    with open(Path(__file__).parent / "definitions" / f"{measure_name}.yaml") as f:
+        measure_yaml = yaml.safe_load(f)
+    return measure_yaml
+
+
+# TODO
+def validate_measure(measure_name):  # pragma: no cover
+    return True
