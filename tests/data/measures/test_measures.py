6543311f9b33e03a35621b05107e79d9b2239734
Tom Ward <tomward@fmail.co.uk>
2026-04-09 11:43:05 +0100

    Test load_measure

---
+++ tests/data/measures/test_measures.py
@@ -0,0 +1,25 @@
+from openprescribing.data.measures import load_measure
+
+
+def test_load_measure():
+    m = load_measure("detemir")
+    assert m == {
+        "metadata": {
+            "title": "Prescribing of Insulin detemir",
+        },
+        "output": {
+            "denominator": "list_size",
+            "numerator": "bnf_items",
+        },
+        "queries": [
+            {
+                "numerator": {
+                    "bnf_codes": {
+                        "included": [
+                            "0601012X0",
+                        ],
+                    },
+                },
+            },
+        ],
+    }
