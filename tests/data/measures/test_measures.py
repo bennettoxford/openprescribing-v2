6b0d12c84bf4394c6571cfeee42f7cf1031d4567
Tom Ward <tomward@fmail.co.uk>
2026-04-09 16:53:50 +0100

    Add more metadata to our sample yaml measures

--- tests/data/measures/test_measures.py
+++ tests/data/measures/test_measures.py
@@ -5,7 +5,27 @@ def test_load_measure():
     m = load_measure("detemir")
     assert m == {
         "metadata": {
+            "tags": [
+                "demo",
+            ],
             "title": "Insulin detemir",
+            "why_it_matters": "Novo Nordisk are [discontinuing all formulations of "
+            "Levemir®](https://www.diabetes.org.uk/about-us/news-and-views/novo-nordisk-to-withdraw-levemir-what-you-need-to-know), "
+            "the only insulin detemir product available in the UK, with supply due "
+            "to exhaust by December 2026.\n"
+            "The Primary Care Diabetes & Obesity Society (PCDOS) and Association "
+            "of British Clinical Diabetologists (ABCD) have [written "
+            "guidance](https://cms.pcdosociety.org/uploads/Levemir_Discontinuation_Guideline_Final_110825_17d277acd3.pdf) "
+            "to support clinicians in selecting and safely initiating alternative "
+            "basal insulins. The guidance recommends that local teams diversify "
+            "prescribing across the available options to reduce supply risks. "
+            "Given the significant regional variation in prescribing, it also "
+            "advises using data to support planning.\n"
+            "This OpenPrescribing measure, alongside it’s [OpenPrescribing "
+            "Hospitals "
+            "counterpart](https://hospitals.openprescribing.net/measures/insulin_detemir/) "
+            "provides data on variation across primary and secondary care to "
+            "support this planning.\n",
         },
         "output": {
             "denominator": "list_size",
