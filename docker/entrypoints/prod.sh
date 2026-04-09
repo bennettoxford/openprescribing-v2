922a50d6ebaca0daa417daa4d7958d545a5749dd
Peter Inglesby <peter.inglesby@gmail.com>
2026-04-09 07:49:22 +0100

    Run migrations in docker entrypoints
    This will ensure that migrations are run on deploy.
    
    An alternative, to run migrations in dokku's release phase, will not
    work because the release phase does not have access to mounted storage.
    
    See this thread for some more discussion:
    
    https://bennettoxford.slack.com/archives/C0A7P1N0B41/p1775625967820689

--- docker/entrypoints/prod.sh
+++ docker/entrypoints/prod.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -euo pipefail
 
-# At the moment the command is simply executed;
-# but any commands that should be run beforehand can be added here
-# (e.g. ./manage.py check or ./manage.py migrate)
-
+./manage.py migrate
 exec "$@"
