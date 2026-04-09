be7dd6a8fac691f13b3c1265a46156e5237c242a
Tom Ward <tomward@fmail.co.uk>
2026-04-09 11:43:05 +0100

    Test load_measure

--- openprescribing/data/measures/measures.py
+++ openprescribing/data/measures/measures.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import yaml
 
 
-def load_measure(measure_name):  # pragma: no cover
+def load_measure(measure_name):
     with open(Path(__file__).parent / "definitions" / f"{measure_name}.yaml") as f:
         measure_yaml = yaml.safe_load(f)
     return measure_yaml
