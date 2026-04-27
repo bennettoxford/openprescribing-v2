3934ca4a1c68c465bc85d17bc2c511128f8d35af
Peter Inglesby <peter.inglesby@gmail.com>
2026-04-27 10:43:19 +0100

    Remove subclasses of Term
    This is in preparation for removing the Term class in the next commit.

--- openprescribing/data/bnf_query.py
+++ openprescribing/data/bnf_query.py
@@ -124,14 +124,12 @@ class BNFQuery:
     @classmethod
     def from_dict(cls, query_dict):
         bnf_codes_dict = query_dict.get("bnf_codes", {"included": [], "excluded": []})
-        included_terms = tuple(
-            [Term.create(rt, False) for rt in bnf_codes_dict["included"]]
-        )
+        included_terms = tuple([Term(rt, False) for rt in bnf_codes_dict["included"]])
         terms = included_terms
 
         if "excluded" in bnf_codes_dict:
             excluded_terms = tuple(
-                [Term.create(rt, True) for rt in bnf_codes_dict["excluded"]]
+                [Term(rt, True) for rt in bnf_codes_dict["excluded"]]
             )
             terms += excluded_terms
 
@@ -274,7 +272,7 @@ class BNFQuery:
 class Term:
     """Represents a term in a query.
 
-    See subclasses for more.
+    This class will be removed in a subsequent commit.
     """
 
     code: str
@@ -288,14 +286,7 @@ class Term:
         else:
             negated = False
             code = raw_term
-        return cls.create(code, negated)
-
-    @staticmethod
-    def create(code, negated):
-        if "_" in code:
-            return StrengthAndFormulationTerm(code, negated)
-        else:
-            return PrefixTerm(code, negated)
+        return cls(code, negated)
 
     def to_param_value(self):
         if self.negated:
@@ -303,47 +294,44 @@ class Term:
         else:
             return self.code
 
-
-@dataclass(frozen=True)
-class PrefixTerm(Term):
-    """Represents a query for all presentations below an object in the BNF hierarchy."""
-
     def build_q(self):
-        return Q(code__startswith=self.code)
+        """Return Q object for finding all presentations that match the given code.
 
-    def describe(self, product_type):
-        description = BNFCode.objects.get(code=self.code).name
-        return {"code": self.code, "description": description}
+        If the code contains an underscore, then it is a "strength and formulation" code,
+        consisting of a BNF chemical substance code (nine characters) and a strength and
+        formulation part (two characters), separated by an underscore.
 
+        For instance, a query for 040702040_AM returns all presentations belonging to the
+        chemical substance 040702040 (Tramadol hydrochloride) that have the same strength
+        and formulation as the generic presentation 040702040AAAMAM (Tramadol 300mg
+        modified-release tablets).
 
-@dataclass(frozen=True)
-class StrengthAndFormulationTerm(Term):
-    """Represents a query for all presentations with a given strength and formulation.
+        If the code does not contain an underscore, then it is a "prefix" code, and matches
+        all presentations beginning with that prefix.
+        """
 
-    A strength and formulation code consists of a BNF chemical substance code (nine
-    characters) and a strength and formulation part (two characters), separated by an
-    underscore.
-
-    For instance, a query for 040702040_AM returns all presentations belonging to the
-    chemical substance 040702040 (Tramadol hydrochloride) that have the same strength
-    and formulation as the generic presentation 040702040AAAMAM (Tramadol 300mg
-    modified-release tablets).
-    """
-
-    def __post_init__(self):
-        prefix, suffix = self.code.split("_")
-        assert len(prefix) == 9  # chemical substance code
-        assert len(suffix) == 2  # strength and formulation part
-
-    def build_q(self):
-        prefix, suffix = self.code.split("_")
-        return Q(code__startswith=prefix, code__endswith=suffix)
-
-    def describe(self, product_type):
-        prefix, suffix = self.code.split("_")
-        generic_code_obj = BNFCode.objects.get(code=f"{prefix}AA{suffix}{suffix}")
-        if product_type == ProductType.ALL:
-            description = f"{generic_code_obj.name} (branded and generic)"
+        if "_" in self.code:
+            prefix, suffix = self.code.split("_")
+            assert len(prefix) == 9  # chemical substance code
+            assert len(suffix) == 2  # strength and formulation part
+            return Q(code__startswith=prefix, code__endswith=suffix)
         else:
-            description = generic_code_obj.name
-        return {"code": self.code, "description": description}
+            return Q(code__startswith=self.code)
+
+    def describe(self, product_type):
+        """Return dict with keys `code` and `description` for describing a search to
+        users."""
+
+        if "_" in self.code:
+            prefix, suffix = self.code.split("_")
+            assert len(prefix) == 9  # chemical substance code
+            assert len(suffix) == 2  # strength and formulation part
+            generic_code_obj = BNFCode.objects.get(code=f"{prefix}AA{suffix}{suffix}")
+            if product_type == ProductType.ALL:
+                description = f"{generic_code_obj.name} (branded and generic)"
+            else:
+                description = generic_code_obj.name
+            return {"code": self.code, "description": description}
+        else:
+            description = BNFCode.objects.get(code=self.code).name
+            return {"code": self.code, "description": description}
