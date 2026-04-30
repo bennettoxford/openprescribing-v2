e0348c0a4f01a1a1d152c93a8e45f2f9e8e2c734
Tom Ward <tomward@fmail.co.uk>
2026-04-30 16:03:24 +0100

    WIP Functional test of multiple queries per analysis
    * alternative implementation needs enhancing

--- tests/data/queries/test_get_org_date_ratio_matrix.py
+++ tests/data/queries/test_get_org_date_ratio_matrix.py
@@ -47,3 +47,30 @@ def test_get_org_date_ratio_matrix_prescribing_vs_prescribing(rxdb, sample_data)
     expected_odm = get_org_date_ratio_matrix_alternative(sample_data, analysis)
 
     assert_approx_equal(odm, expected_odm)
+
+
+@pytest.mark.django_db(databases=["data"])
+def test_get_org_date_ratio_matrix_prescribing_vs_list_size_multiple_queries(
+    rxdb, sample_data
+):
+    analysis = Analysis(
+        queries=(
+            AnalysisQuery(
+                ntr_query=BNFQuery(bnf_codes=["1001030U0AAABAB"]),
+                dtr_query=ListSizeQuery(),
+            ),
+            AnalysisQuery(
+                ntr_query=BNFQuery(bnf_codes=["1001030U0AAACAC"]),
+                dtr_query=ListSizeQuery(),
+            ),
+        ),
+        org_id="ICB01",
+    )
+
+    with rxdb.get_cursor() as cursor:
+        odm = get_org_date_ratio_matrix(cursor, analysis)
+
+    # TODO: upgrade alternative implementation
+    expected_odm = get_org_date_ratio_matrix_alternative(sample_data, analysis)
+
+    assert_approx_equal(odm, expected_odm)
