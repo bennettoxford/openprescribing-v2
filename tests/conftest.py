9cb9b86ef3f31d3ab00a67f095563f4db121b2f7
Peter Inglesby <peter.inglesby@gmail.com>
2026-06-16 19:04:39 +0100

    Extract generate_test_data function
    We're going to reuse the logic to create prescribing for other sets of
    BNF codes.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,10 @@
-import datetime
 import itertools
 
 import pytest
 
 import openprescribing.data.rxdb
-from openprescribing.data.models import BNFCode, Org, OrgRelation
+from openprescribing.data.models import BNFCode
+from tests.utils.data_utils import generate_test_data
 from tests.utils.ingest_utils import ingest_dmd_bnf_map_data, ingest_dmd_data
 from tests.utils.medications_utils import MedicationsBuilder
 from tests.utils.rxdb_utils import RXDBFixture
@@ -147,52 +147,12 @@ def sample_data(rxdb, bnf_codes):
 
     This data should be just about rich enough for the tests of the website and API.
     """
-
-    list_size_data = []
-    prescribing_data = []
-
-    for icb_ix in range(2):
-        icb = Org.objects.create(
-            id=f"ICB{icb_ix:02}",
-            name=f"ICB {icb_ix}",
-            org_type=Org.OrgType.ICB,
-        )
-        for pra_ix in range(2):
-            pra = Org.objects.create(
-                id=f"PRA{icb_ix}{pra_ix}",
-                name=f"Practice {icb_ix}{pra_ix}",
-                org_type=Org.OrgType.PRACTICE,
-            )
-            OrgRelation.objects.create(child=pra, parent=icb)
-            for month in (1, 2, 3):
-                date = datetime.date(2025, month, 1)
-                list_size_data.append(
-                    {
-                        "date": date,
-                        "practice_code": pra.id,
-                        "total": 1000 + 100 * icb_ix + 10 * pra_ix + month,
-                    }
-                )
-                for bnf_ix, bnf_code in enumerate(
-                    [
-                        "1001030U0AAABAB",
-                        "1001030U0AAACAC",
-                        "1001030U0BDAAAB",
-                        "1001030U0BDABAC",
-                    ]
-                ):
-                    prescribing_data.append(
-                        {
-                            "date": date,
-                            "bnf_code": bnf_code,
-                            "practice_code": pra.id,
-                            "items": 8 * bnf_ix + 4 * icb_ix + 2 * pra_ix + month,
-                        },
-                    )
-
-    rxdb.ingest(
-        list_size_data=list_size_data,
-        prescribing_data=prescribing_data,
+    return generate_test_data(
+        rxdb,
+        [
+            "1001030U0AAABAB",
+            "1001030U0AAACAC",
+            "1001030U0BDAAAB",
+            "1001030U0BDABAC",
+        ],
     )
-
-    return {"list_size_data": list_size_data, "prescribing_data": prescribing_data}
