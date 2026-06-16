ef918a38563be6d17d14260fc931babed6a69697
Peter Inglesby <peter.inglesby@gmail.com>
2026-06-16 19:04:39 +0100

    Support adding AMPs via medications fixture

--- tests/utils/medications_utils.py
+++ tests/utils/medications_utils.py
@@ -1,13 +1,17 @@
 from openprescribing.data.models import (
+    AMP,
     VMP,
     VPI,
     VTM,
+    AvailabilityRestriction,
     BasisOfName,
     BNFCode,
     DmdBnfMap,
     Ing,
+    LicensingAuthority,
     Ont,
     OntFormRoute,
+    Supplier,
     VirtualProductPresStatus,
 )
 
@@ -17,8 +21,6 @@ class MedicationsBuilder:
     the underlying tables.
 
     It can be used in tests via the medications fixture.
-
-    At the moment we only support adding VMPs.  Adding AMPs would be straightforward.
     """
 
     def __init__(self):
@@ -27,7 +29,12 @@ class MedicationsBuilder:
         self._form_route_cds = {}
 
     def add_rows(self, rows):
-        """This method should be called in tests to populate the underlying tables."""
+        """This method should be called in tests to populate the underlying tables.
+
+        Each row is a dict describing either a VMP or an AMP.  A row with a truthy
+        `is_amp` key is added as an AMP belonging to an existing VMP (identified by its
+        `vmp_id`); any other row is added as a VMP.
+        """
 
         # There are non-nullable FKs from VMP to these tables, so we need to have
         # records in the database.
@@ -35,9 +42,13 @@ class MedicationsBuilder:
         VirtualProductPresStatus.objects.get_or_create(cd=0, defaults={"descr": ""})
 
         for row in rows:
-            self._add_row(**row)
+            row = dict(row)
+            if row.pop("is_amp", False):
+                self._add_amp(**row)
+            else:
+                self._add_vmp(**row)
 
-    def _add_row(
+    def _add_vmp(
         self,
         *,
         bnf_code=None,
@@ -82,6 +93,40 @@ class MedicationsBuilder:
         for descr in form_routes:
             formcd = self._get_or_create_form_route_cd(descr)
             Ont.objects.create(vmp_id=dmd_id, form_id=formcd)
+
+    def _add_amp(self, *, vmp_id, bnf_code=None, name="", invalid=False):
+        # There are non-nullable FKs from AMP to these tables, so we need to have
+        # records in the database.
+        Supplier.objects.get_or_create(cd=0, defaults={"descr": "", "invalid": False})
+        LicensingAuthority.objects.get_or_create(cd=0, defaults={"descr": ""})
+        AvailabilityRestriction.objects.get_or_create(cd=0, defaults={"descr": ""})
+
+        apid = self._next_id
+        self._next_id += 1
+
+        # An AMP shares its VMP's BNF code unless one is given explicitly.
+        if bnf_code is None:  # pragma: no cover
+            bnf_code = DmdBnfMap.objects.get(dmd_id=vmp_id).bnf_code
+
+        BNFCode.objects.get_or_create(
+            code=bnf_code,
+            defaults={"name": "", "level": BNFCode.Level.PRESENTATION},
+        )
+
+        DmdBnfMap.objects.create(dmd_id=apid, bnf_code=bnf_code)
+
+        AMP.objects.create(
+            apid=apid,
+            vmp_id=vmp_id,
+            nm=name,
+            descr=name,
+            invalid=invalid,
+            supp_id=0,
+            lic_auth_id=0,
+            avail_restrict_id=0,
+            ema=False,
+            parallel_import=False,
+        )
 
     def _get_or_create_form_route_cd(self, descr):
         # Assign a stable cd to each form/route description so that the medications
