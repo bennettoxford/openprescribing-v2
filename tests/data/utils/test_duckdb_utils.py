455cef3c56a9133fd7aa7abdcfb7485a3bb66900
Tom Ward <tomward@fmail.co.uk>
2026-07-01 11:07:44 +0100

    The results of iterdir() are unordered
    * order them, for a reliable test!

--- tests/data/utils/test_duckdb_utils.py
+++ tests/data/utils/test_duckdb_utils.py
@@ -10,6 +10,6 @@ def test_profiling_connection(tmp_path):
     conn.sql("SELECT 1")
     conn.sql("SELECT 2")
     conn.close()
-    q1, q2 = [json.loads(q.read_text()) for q in tmp_path.iterdir()]
+    q1, q2 = [json.loads(q.read_text()) for q in sorted(tmp_path.iterdir())]
     assert q1["query_name"] == "SELECT 1"
     assert q2["query_name"] == "SELECT 2"
